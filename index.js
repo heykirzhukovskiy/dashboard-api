089f10f39d75917b2c501a207755ae7a2f8c61b7
heykirzhukovskiy <zhuwork@yandex.ru>
2022-01-12 11:51:39 +0300

    Adds error handler

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 import express from 'express'
+import { userRouter } from './users/users.js'
 
 const port = 8000
 const app = express()
 
-app.all('/hello', (req, res, next) => {
-	console.log('All!')
+// app.all('/hello', (req, res, next) => {
+// 	console.log('All!')
+// 	next()
+// })
+
+// const cb = (req, res, next) => {
+// 	console.log('CB')
+// 	next()
+// }
+
+app.use((req, res, next) => {
+	console.log('Time ', Date.now())
 	next()
 })
 
-const cb = (req, res, next) => {
-	console.log('CB')
-	next()
-}
+app.use('/users', userRouter)
 
-app.get('/hello', cb, (req, res) => {
-	res.send('Hi!')
+app.get('/hello', (req, res) => {
+	throw new Error('Error!!!')
+	// res.status(203).json({
+	// 	success: true,
+	// 	message: 'hello',
+	// })
+	//
+	// res.links({
+	// 	next: 'index.js',
+	// })
+	//
+	// res.send('hello')
+	//
+	// res.download('../../../Downloads/Telegram Desktop/02_ART.jpg', 'Beautiful pic')
 })
 
-app
-	.route('/user')
-	.get('/hello', (req, res) => {
-		res.send('Hi!')
-	})
-	.post('/hello', (req, res) => {
-		res.send('Hi!')
-	})
+// app
+// 	.route('/user')
+// 	.get('/hello', (req, res) => {
+// 		res.send('Hi!')
+// 	})
+// 	.post('/hello', cb, (req, res) => {
+// 		res.send('Hi!')
+// 	})
+
+app.use((err, req, res, next) => {
+	console.log(err.message)
+	res.status(500).send(err.message)
+})
 
 app.listen(port, () => {
 	console.log(`Server started at http://localhost/${port}`)
