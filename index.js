5e7904ad643a033b130c4647dabcbe12860d6154
heykirzhukovskiy <zhuwork@yandex.ru>
2022-01-12 04:55:39 +0300

    first of all

---
+++ index.js
@@ -0,0 +1,12 @@
+import http from 'http'
+
+const host = '127.0.0.1'
+const port = 8000
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'text-plain')
+	res.end('Hi!')
+})
+
+server.listen(port, host, () => console.log(`Server started at http://${host}:${port}`))
